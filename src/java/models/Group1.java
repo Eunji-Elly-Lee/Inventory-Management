cf95957a8b76c5119e0f22aa276f350640b5c1dd
849747 <eunji.lee@edu.sait.ca>
2022-01-12 22:23:06 -0700

    Create entity classes

---
+++ src/java/models/Group1.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author 849747
+ */
+@Entity
+@Table(name = "group")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Group1.findAll", query = "SELECT g FROM Group1 g")
+    , @NamedQuery(name = "Group1.findByGroupId", query = "SELECT g FROM Group1 g WHERE g.groupId = :groupId")
+    , @NamedQuery(name = "Group1.findByGroupName", query = "SELECT g FROM Group1 g WHERE g.groupName = :groupName")})
+public class Group1 implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "group_id")
+    private Integer groupId;
+    @Basic(optional = false)
+    @Column(name = "group_name")
+    private String groupName;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "group1", fetch = FetchType.EAGER)
+    private List<User> userList;
+
+    public Group1() {
+    }
+
+    public Group1(Integer groupId) {
+        this.groupId = groupId;
+    }
+
+    public Group1(Integer groupId, String groupName) {
+        this.groupId = groupId;
+        this.groupName = groupName;
+    }
+
+    public Integer getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Integer groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    @XmlTransient
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (groupId != null ? groupId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Group1)) {
+            return false;
+        }
+        Group1 other = (Group1) object;
+        if ((this.groupId == null && other.groupId != null) || (this.groupId != null && !this.groupId.equals(other.groupId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "models.Group1[ groupId=" + groupId + " ]";
+    }
+    
+}
