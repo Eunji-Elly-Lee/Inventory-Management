0a981eb0edb53158f1f16113cf3400d9f1435bf2
849747 <eunji.lee@edu.sait.ca>
2022-01-13 19:48:46 -0700

    Update inventorydb.sql (delete entity files)

--- src/java/database/inventorydb.sql
+++ src/java/database/inventorydb.sql
@@ -2,7 +2,7 @@
 ** Author: Eunji Elly Lee
 ** Created: Jan 11, 2022
 ** Description: As a database of inventories, it creates and manages tables
-** that contain information about users, roles, groups, items, and categories.
+** that contain information about users, roles, families, items, and categories.
 **************************************************************************** */
 
 DROP SCHEMA IF EXISTS `inventorydb`;
@@ -26,12 +26,12 @@ CREATE TABLE IF NOT EXISTS `inventorydb`.`role` (
   PRIMARY KEY (`role_id`));
 
 -- -----------------------------------------------------
--- Table `inventorydb`.`group`
+-- Table `inventorydb`.`family`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `inventorydb`.`group` (
-  `group_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `group_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`group_id`));
+CREATE TABLE IF NOT EXISTS `inventorydb`.`family` (
+  `family_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `family_name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`family_id`));
 
 -- -----------------------------------------------------
 -- Table `inventorydb`.`user`
@@ -42,16 +42,16 @@ CREATE TABLE IF NOT EXISTS `inventorydb`.`user` (
   `first_name` VARCHAR(20) NOT NULL,
   `last_name` VARCHAR(20) NOT NULL,
   `password` VARCHAR(20) NOT NULL,
-  `group` INT(11) NOT NULL,
+  `family` INT(11) NOT NULL,
   `role` INT(11) NOT NULL,
   `uuid` VARCHAR(50),
   PRIMARY KEY (`email`),
   CONSTRAINT `fk_user_role`
     FOREIGN KEY (`role`)
     REFERENCES `inventorydb`.`role` (`role_id`),
-  CONSTRAINT `fk_user_group`
-    FOREIGN KEY (`group`)
-    REFERENCES `inventorydb`.`group` (`group_id`));
+  CONSTRAINT `fk_user_family`
+    FOREIGN KEY (`family`)
+    REFERENCES `inventorydb`.`family` (`family_id`));
 
 -- -----------------------------------------------------
 -- Table `inventorydb`.`item`
@@ -73,7 +73,7 @@ CREATE TABLE IF NOT EXISTS `inventorydb`.`item` (
 
 INSERT INTO `role` VALUES (1, 'system admin');
 INSERT INTO `role` VALUES (2, 'regular user');
-INSERT INTO `role` VALUES (3, 'group admin');
+INSERT INTO `role` VALUES (3, 'family admin');
 
 INSERT INTO `category` (`category_name`) VALUES ('kitchen');
 INSERT INTO `category` (`category_name`) VALUES ('washroom');
@@ -84,21 +84,21 @@ INSERT INTO `category` (`category_name`) VALUES ('garage');
 INSERT INTO `category` (`category_name`) VALUES ('office');
 INSERT INTO `category` (`category_name`) VALUES ('other');
 
-INSERT INTO `group` (`group_name`) VALUES ('SYSTEM');
-INSERT INTO `group` (`group_name`) VALUES ('RED');
-INSERT INTO `group` (`group_name`) VALUES ('BLUE');
+INSERT INTO `family` (`family_name`) VALUES ('SYSTEM');
+INSERT INTO `family` (`family_name`) VALUES ('RED');
+INSERT INTO `family` (`family_name`) VALUES ('BLUE');
 
-INSERT INTO `user` (`email`,`active`,`first_name`,`last_name`,`password`,`group`,`role`)
+INSERT INTO `user` (`email`,`active`,`first_name`,`last_name`,`password`,`family`,`role`)
 	VALUES ('cprg.352d+admin@gmail.com', true, 'Admin', 'Admin', 'password', 1, 1);
-INSERT INTO `user` (`email`,`active`,`first_name`,`last_name`,`password`,`group`,`role`)
+INSERT INTO `user` (`email`,`active`,`first_name`,`last_name`,`password`,`family`,`role`)
 	VALUES ('cprg.352d+admin2@gmail.com', true, 'Admin2', 'Admin2', 'password', 2, 3);
-INSERT INTO `user` (`email`,`active`,`first_name`,`last_name`,`password`,`group`,`role`)
+INSERT INTO `user` (`email`,`active`,`first_name`,`last_name`,`password`,`family`,`role`)
 	VALUES ('cprg.352d+anne@gmail.com', true, 'Anne', 'Annerson', 'password', 2, 2);
-INSERT INTO `user` (`email`,`active`,`first_name`,`last_name`,`password`,`group`,`role`)
+INSERT INTO `user` (`email`,`active`,`first_name`,`last_name`,`password`,`family`,`role`)
 	VALUES ('cprg.352d+barb@gmail.com', true, 'Barb', 'Barber', 'password', 2, 2);
-INSERT INTO `user` (`email`,`active`,`first_name`,`last_name`,`password`,`group`,`role`)
+INSERT INTO `user` (`email`,`active`,`first_name`,`last_name`,`password`,`family`,`role`)
 	VALUES ('cprg.352d+admin3@gmail.com', true, 'Admin3', 'Admin3', 'password', 3, 3);
-INSERT INTO `user` (`email`,`active`,`first_name`,`last_name`,`password`,`group`,`role`)
+INSERT INTO `user` (`email`,`active`,`first_name`,`last_name`,`password`,`family`,`role`)
 	VALUES ('cprg.352d+elly@gmail.com', true, 'Elly', 'Lee', 'password', 3, 2);
 
 INSERT INTO `item` (`category`,`item_name`,`price`,`owner`) VALUES (1, 'blender', 29.99, 'cprg.352d+anne@gmail.com');
