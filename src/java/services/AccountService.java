8ec4c3d7b3ac8297b6b371f3048a440cd715d22e
849747 <eunji.lee@edu.sait.ca>
2022-01-20 22:59:52 -0700

    Update AccountService.java

--- src/java/services/AccountService.java
+++ src/java/services/AccountService.java
@@ -1,6 +1,7 @@
 package services;
 
 import dataaccess.UserDB;
+import java.util.*;
 import models.User;
 
 /**
@@ -29,5 +30,45 @@ public class AccountService {
         User user = userDB.get(email);
         
         return user;
+    }
+    
+    public void resetPassword(String email, String path, String url) {
+        UserDB userDB = new UserDB();
+        
+        try {
+            User beforeUser = userDB.get(email);
+            User afterUser = userDB.get(email);
+            
+            String uuid = UUID.randomUUID().toString();
+            String link = url + "?uuid=" + uuid;        
+            String to = beforeUser.getEmail();
+            String subject = "Home nVentory Password";
+            String template = path + "/emailTemplates/resetPassword.html";
+               
+            HashMap<String, String> tags = new HashMap<>();
+            tags.put("firstname", beforeUser.getFirstName());
+            tags.put("lastname", beforeUser.getLastName());
+            tags.put("link", link);
+            
+            afterUser.setUuid(uuid);
+            userDB.update(beforeUser, afterUser);
+            
+            GmailService.sendMail(to, subject, template, tags);
+        } catch(Exception e) {}
+    }
+    
+    public boolean changePassword(String uuid, String password) {
+        UserDB userDB = new UserDB();
+        
+        try {
+            User beforeUser = userDB.getByUUID(uuid);
+            User afterUser = userDB.getByUUID(uuid);
+            afterUser.setPassword(password);
+            afterUser.setUuid(null);
+            userDB.update(beforeUser, afterUser);            
+            return true;
+        } catch(Exception e) {
+            return false;
+        }
     }
 }
