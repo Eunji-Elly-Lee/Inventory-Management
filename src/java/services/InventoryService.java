d235f33724cc2c3b6756ff939ae5abe8e3678353
849747 <eunji.lee@edu.sait.ca>
2022-01-31 22:37:13 -0700

    Create InventoryService.java

---
+++ src/java/services/InventoryService.java
@@ -0,0 +1,70 @@
+package services;
+
+import dataaccess.*;
+import java.util.List;
+import java.util.logging.*;
+import models.*;
+
+/**
+ * The java file of a service layer for managing all activities related to an inventory.
+ * @author Eunji Elly Lee
+ * @version Jan 31, 2022
+ */
+public class InventoryService {
+    public Item getItem(int itemID) throws Exception {
+        ItemDB itemDB = new ItemDB();
+        Item item = itemDB.get(itemID);
+        
+        return item;
+    }
+    
+    public List<Category> getAllCategories() throws Exception {
+        CategoryDB categoryDB = new CategoryDB();
+        List<Category> categories = categoryDB.getAll();
+        
+        return categories;
+    }
+    
+    public void insertItem(String email, int categoryID, String itemName, double price)
+            throws Exception {
+        UserDB userDB = new UserDB();
+        User user = userDB.get(email);
+        
+        CategoryDB categoryDB = new CategoryDB();
+        Category category = categoryDB.get(categoryID);  
+        
+        ItemDB itemDB = new ItemDB();              
+        Item item = new Item(0, itemName, price);        
+        item.setOwner(user);
+        item.setCategory(category);
+        
+        itemDB.insert(item);
+    }
+    
+    public void updateItem(int itemID, int categoryID, String itemName, double price)
+            throws Exception {
+        CategoryDB categoryDB = new CategoryDB();
+        Category category = categoryDB.get(categoryID);  
+        
+        ItemDB itemDB = new ItemDB();
+        Item beforeItem = itemDB.get(itemID);
+        Item afterItem = itemDB.get(itemID);
+        afterItem.setItemName(itemName);
+        afterItem.setPrice(price);
+        afterItem.setCategory(category);
+        
+        itemDB.update(beforeItem, afterItem);
+    }
+    
+    public void deleteItem(String email, int itemID) throws Exception {
+        ItemDB itemDB = new ItemDB();
+        Item item = itemDB.get(itemID);
+        
+        if(!item.getOwner().getEmail().equals(email)) {
+            Logger.getLogger(InventoryService.class.getName()).log(Level.SEVERE, null,
+                    "users cannot delete inventory items they do not own");
+        } else {
+            itemDB.delete(item);
+        }
+    }
+}
