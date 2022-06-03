2e0d887371fe9aaec5d161b809963060fa2d249e
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-06-03 03:38:52 -0600

    Update JavaMail function

--- src/java/services/JavaMailService.java
+++ src/java/services/JavaMailService.java
@@ -10,9 +10,9 @@ import javax.naming.*;
 /**
  * The java file of a service layer for managing all activities related to a mailing.
  * @author Eunji Elly Lee
- * @version Jan 20, 2022
+ * @version June 3, 2022
  */
-public class GmailService {
+public class JavaMailService {
     public static void sendMail(String to, String subject, String template,
             HashMap<String, String> tags) throws Exception {
         String body = "";
@@ -29,7 +29,7 @@ public class GmailService {
                 body = body.replace("{{" + key + "}}", tags.get(key));
             }
         } catch(Exception e) {
-            Logger.getLogger(GmailService.class.getName()).log(Level.SEVERE, null, e);
+            Logger.getLogger(JavaMailService.class.getName()).log(Level.SEVERE, null, e);
         }
 
         sendMail(to, subject, body, true);
@@ -40,19 +40,22 @@ public class GmailService {
         Context env = (Context) new InitialContext().lookup("java:comp/env");
         String username = (String) env.lookup("webmail-username");
         String password = (String) env.lookup("webmail-password");
-
+        
         Properties props = new Properties();
-        props.put("mail.transport.protocol", "smtps");
-        props.put("mail.smtps.host", "smtp.gmail.com");
-        props.put("mail.smtps.port", 465);
-        props.put("mail.smtps.auth", "true");
-        props.put("mail.smtps.quitwait", "false");
-        Session session = Session.getDefaultInstance(props);
-        session.setDebug(true);
-
+        props.put("mail.smtp.host", "smtp.office365.com"); 
+        props.put("mail.smtp.port", 587); 
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        
+        Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator(){
+            protected PasswordAuthentication getPasswordAuthentication() {
+		return new PasswordAuthentication(username, password);
+            }
+	});
+        
         Message message = new MimeMessage(session);
         message.setSubject(subject);
-        
+                
         if(bodyIsHTML) {
             message.setContent(body, "text/html");
         } else {
@@ -63,10 +66,7 @@ public class GmailService {
         Address toAddress = new InternetAddress(to);
         message.setFrom(fromAddress);
         message.setRecipient(Message.RecipientType.TO, toAddress);
-
-        Transport transport = session.getTransport();
-        transport.connect(username, password);
-        transport.sendMessage(message, message.getAllRecipients());
-        transport.close();
+        
+        Transport.send(message);     
     }
 }
